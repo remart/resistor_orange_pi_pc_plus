1c116990ae73fb7441a163baccf7bbe95a9a2499
REMART MARANAN <75769151+remart@users.noreply.github.com>
2022-02-11 19:43:56 +0800

    Add files via upload

---
+++ ULTRASONIC_SENSOR.py
@@ -0,0 +1,39 @@
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+import time
+
+#pins that will connect hc-sr04 to the orange pi pcb
+TRIG = port.PA21
+ECHO = port.PC3
+
+gpio.init()
+
+print("Distance Measurement In Progress")
+
+gpio.setcfg(TRIG, gpio.OUTPUT)
+gpio.setcfg(ECHO, gpio.INPUT)
+
+gpio.output(TRIG, 0)
+
+print("Waiting For Sensor To Settle")
+
+for i in range(1, 20)
+	time.sleep(1)
+	gpio.output(TRIG, 1)
+	time.sleep(0.00001)
+	gpio.output(TRIG, 0)
+
+
+	while gpio.input(ECHO) == 0:
+	    pulse_start = time.time()
+
+	while gpio.input(ECHO) == 1:
+	    pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance = pulse_duration * 17150
+
+	distance = round(distance, 2)
+
+	print("Distance: " + str(distance) + "cm")
